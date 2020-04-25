681f56a40b8d9b6d7e2038d0f0078f220cbfd622
Sean <sean gauer>
2020-04-25 09:37:50 -0600

    ops

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,9 +23,16 @@ myFunction();
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-function sumation(value){
-  let x = 0;
-  return funciton ();
-    x = x + 1;
-    return x;
+function summation( value )
+{
+  let total = 0;
+  return function counter()
+  {
+   for( let i = 1; 1 <= value; i++ )
+    total += i;
+    return total;
   }
+}
+
+let counter = summation( 4 );
+console.log(counter() );
